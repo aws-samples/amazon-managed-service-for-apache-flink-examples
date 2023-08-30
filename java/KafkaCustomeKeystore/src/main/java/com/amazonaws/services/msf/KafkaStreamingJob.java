343a161e48974ff96d92be3023a53d6d0f051fbb
Lorenzo Nicora <nicorlor@amazon.co.uk>
2023-08-30 11:11:38 +0100

    Import cleanup

--- java/KafkaCustomeKeystore/src/main/java/com/amazonaws/services/msf/KafkaStreamingJob.java
+++ java/KafkaCustomeKeystore/src/main/java/com/amazonaws/services/msf/KafkaStreamingJob.java
@@ -6,12 +6,12 @@ import org.apache.flink.api.common.serialization.SimpleStringSchema;
 import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.connector.base.DeliveryGuarantee;
 import org.apache.flink.connector.kafka.sink.KafkaRecordSerializationSchema;
+import org.apache.flink.connector.kafka.sink.KafkaSink;
+import org.apache.flink.connector.kafka.source.KafkaSource;
 import org.apache.flink.connector.kafka.source.enumerator.initializer.OffsetsInitializer;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.LocalStreamEnvironment;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-import org.apache.flink.connector.kafka.sink.KafkaSink;
-import org.apache.flink.connector.kafka.source.KafkaSource;
 
 import java.io.IOException;
 import java.util.HashMap;
