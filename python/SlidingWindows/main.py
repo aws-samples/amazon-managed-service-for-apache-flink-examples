5e3630355d8208b22b7536c68bbb71a88c09cd3a
Dziolak, Krzysztof <dziolakk@amazon.com>
2024-05-21 09:04:44 +0100

    Adapt Python Windowing examples to automation standards

--- python/SlidingWindows/main.py
+++ python/SlidingWindows/main.py
@@ -14,12 +14,12 @@ This module:
     5. These sliding window results are inserted into the Sink table.
 """
 
+import json
+import os
 from pyflink.table import EnvironmentSettings, TableEnvironment, DataTypes
-from pyflink.table.window import Slide
 from pyflink.table.expressions import col, lit
 from pyflink.table.udf import udf
-import os
-import json
+from pyflink.table.window import Slide
 
 # 1. Creates a Table Environment
 env_settings = EnvironmentSettings.in_streaming_mode()
@@ -58,28 +58,26 @@ def property_map(props, property_group_id):
             return prop["PropertyMap"]
 
 
-def create_input_table(table_name, stream_name, region, stream_initpos):
+def create_input_table(table_name):
     return """ CREATE TABLE {0} (
                 ticker VARCHAR(6),
-                price DOUBLE,
+                price NUMERIC(6,2),
                 event_time TIMESTAMP(3),
                 WATERMARK FOR event_time AS event_time - INTERVAL '5' SECOND
               )
               PARTITIONED BY (ticker)
               WITH (
-                'connector' = 'kinesis',
-                'stream' = '{1}',
-                'aws.region' = '{2}',
-                'scan.stream.initpos' = '{3}',
-                'format' = 'json',
-                'json.timestamp-format.standard' = 'ISO-8601'
-              ) """.format(table_name, stream_name, region, stream_initpos)
+                'connector' = 'datagen',
+                'fields.ticker.length' = '3',
+                'fields.price.min' = '50',
+                'fields.price.max' = '200'
+              ) """.format(table_name)
 
 
 def create_output_table(table_name, stream_name, region):
     return """ CREATE TABLE {0} (
                 ticker VARCHAR(6),
-                price DOUBLE,
+                price NUMERIC(6,2),
                 event_time VARCHAR(64)
               )
               PARTITIONED BY (ticker)
@@ -122,14 +120,9 @@ table_env.create_temporary_system_function("to_string", to_string)
 
 def main():
     # Application Property Keys
-    input_property_group_key = "consumer.config.0"
-    producer_property_group_key = "producer.config.0"
+    producer_property_group_key = "OutputStream0"
 
-    input_stream_key = "input.stream.name"
-    input_region_key = "aws.region"
-    input_starting_position_key = "scan.stream.initpos"
-
-    output_stream_key = "output.stream.name"
+    output_stream_key = "stream.name"
     output_region_key = "aws.region"
 
     # tables
@@ -139,18 +132,12 @@ def main():
     # get application properties
     props = get_application_properties()
 
-    input_property_map = property_map(props, input_property_group_key)
     output_property_map = property_map(props, producer_property_group_key)
-
-    input_stream = input_property_map[input_stream_key]
-    input_region = input_property_map[input_region_key]
-    stream_initpos = input_property_map[input_starting_position_key]
-
     output_stream = output_property_map[output_stream_key]
     output_region = output_property_map[output_region_key]
 
     # 2. Creates a source table from a Kinesis Data Stream
-    table_env.execute_sql(create_input_table(input_table_name, input_stream, input_region, stream_initpos))
+    table_env.execute_sql(create_input_table(input_table_name))
 
     # 3. Creates a sink table writing to a Kinesis Data Stream
     table_env.execute_sql(create_output_table(output_table_name, output_stream, output_region))
--- python/TumblingWindows/main.py
+++ python/TumblingWindows/main.py
@@ -3,7 +3,7 @@
 # -*- coding: utf-8 -*-
 
 """
-tumbling-windows.py
+main.py
 ~~~~~~~~~~~~~~~~~~~
 This module:
     1. Creates a table environment
@@ -58,27 +58,25 @@ def property_map(props, property_group_id):
             return prop["PropertyMap"]
 
 
-def create_input_table(table_name, stream_name, region, stream_initpos):
+def create_input_table(table_name):
     return """ CREATE TABLE {0} (
                 ticker VARCHAR(6),
-                price DOUBLE,
+                price NUMERIC(6,2),
                 event_time TIMESTAMP(3),
                 WATERMARK FOR event_time AS event_time - INTERVAL '5' SECOND
               )
               PARTITIONED BY (ticker)
               WITH (
-                'connector' = 'kinesis',
-                'stream' = '{1}',
-                'aws.region' = '{2}',
-                'scan.stream.initpos' = '{3}',
-                'format' = 'json',
-                'json.timestamp-format.standard' = 'ISO-8601'
-              ) """.format(table_name, stream_name, region, stream_initpos)
+                'connector' = 'datagen',
+                'fields.ticker.length' = '3',
+                'fields.price.min' = '50',
+                'fields.price.max' = '200'
+              ) """.format(table_name)
 
 def create_output_table(table_name, stream_name, region):
     return """ CREATE TABLE {0} (
                 ticker VARCHAR(6),
-                price DOUBLE,
+                price NUMERIC(6,2),
                 event_time VARCHAR(64)
               )
               PARTITIONED BY (ticker)
@@ -115,14 +113,9 @@ table_env.create_temporary_system_function("to_string", to_string)
 
 def main():
     # Application Property Keys
-    input_property_group_key = "consumer.config.0"
-    producer_property_group_key = "producer.config.0"
+    producer_property_group_key = "OutputStream0"
 
-    input_stream_key = "input.stream.name"
-    input_region_key = "aws.region"
-    input_starting_position_key = "scan.stream.initpos"
-
-    output_stream_key = "output.stream.name"
+    output_stream_key = "stream.name"
     output_region_key = "aws.region"
 
     # tables
@@ -132,18 +125,13 @@ def main():
     # get application properties
     props = get_application_properties()
 
-    input_property_map = property_map(props, input_property_group_key)
     output_property_map = property_map(props, producer_property_group_key)
-
-    input_stream = input_property_map[input_stream_key]
-    input_region = input_property_map[input_region_key]
-    stream_initpos = input_property_map[input_starting_position_key]
 
     output_stream = output_property_map[output_stream_key]
     output_region = output_property_map[output_region_key]
 
     # 2. Creates a source table from a Kinesis Data Stream
-    table_env.execute_sql(create_input_table(input_table_name, input_stream, input_region, stream_initpos))
+    table_env.execute_sql(create_input_table(input_table_name))
 
     # 3. Creates a sink table writing to a Kinesis Data Stream
     table_env.execute_sql(create_output_table(output_table_name, output_stream, output_region))
