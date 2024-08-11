d929a0f5a264c6fdae8fc34565b2940e90a42448
“Ravtej” <sravtej@amazon.co.uk>
2024-08-11 17:37:10 +0100

    Updated to load application properties from flink-application-properties-dev.json instead of command line when running locally

--- java/KafkaCustomKeystoreWithConfigProviders/src/main/java/com/amazonaws/services/msf/StreamingJob.java
+++ java/KafkaCustomKeystoreWithConfigProviders/src/main/java/com/amazonaws/services/msf/StreamingJob.java
@@ -1,10 +1,13 @@
 package com.amazonaws.services.msf;
 
 
-import com.amazonaws.services.kinesisanalytics.runtime.KinesisAnalyticsRuntime;
+import java.io.IOException;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Properties;
+
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
 import org.apache.flink.api.common.serialization.SimpleStringSchema;
-import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.connector.kafka.source.KafkaSource;
 import org.apache.flink.connector.kafka.source.KafkaSourceBuilder;
 import org.apache.flink.connector.kafka.source.enumerator.initializer.OffsetsInitializer;
@@ -15,10 +18,7 @@ import org.apache.flink.util.Preconditions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Properties;
+import com.amazonaws.services.kinesisanalytics.runtime.KinesisAnalyticsRuntime;
 
 public class StreamingJob {
     private static final Logger LOG = LoggerFactory.getLogger(StreamingJob.class);
@@ -26,44 +26,48 @@ public class StreamingJob {
     private static final String DEFAULT_SOURCE_TOPIC = "source";
     private static final String DEFAULT_CONSUMER_GROUP = "flink-app";
 
-    public static final String MSKBOOTSTRAP_SERVERS_KEY = "MSKBootstrapServers";
-    public static final String KAFKA_SOURCE_TOPIC_KEY = "KafkaSourceTopic";
-    public static final String KAFKA_CONSUMER_GROUP_ID_KEY = "KafkaConsumerGroupId";
-    public static final String S3_BUCKET_REGION_KEY = "S3BucketRegion";
-    public static final String KEYSTORE_S3_BUCKET_KEY = "KeystoreS3Bucket";
-    public static final String KEYSTORE_S3_PATH_KEY = "KeystoreS3Path";
-    public static final String TRUSTSTORE_S3_BUCKET_KEY = "TruststoreS3Bucket";
-    public static final String TRUSTSTORE_S3_PATH_KEY = "TruststoreS3Path";
-    public static final String KEYSTORE_PASS_SECRET_KEY = "KeystorePassSecret";
-    public static final String KEYSTORE_PASS_SECRET_FIELD_KEY = "KeystorePassSecretField";
+    public static final String KAFKA_SOURCE_TOPIC_KEY = "topic";
+    public static final String MSKBOOTSTRAP_SERVERS_KEY = "bootstrap.servers";
+    public static final String KAFKA_CONSUMER_GROUP_ID_KEY = "group.id";
+    public static final String S3_BUCKET_REGION_KEY = "bucket.region";
+    public static final String KEYSTORE_S3_BUCKET_KEY = "keystore.bucket";
+    public static final String KEYSTORE_S3_PATH_KEY = "keystore.path";
+    public static final String TRUSTSTORE_S3_BUCKET_KEY = "truststore.bucket";
+    public static final String TRUSTSTORE_S3_PATH_KEY = "truststore.path";
+    public static final String KEYSTORE_PASS_SECRET_KEY = "keystore.secret";
+    public static final String KEYSTORE_PASS_SECRET_FIELD_KEY = "keystore.secret.field";
+
+    private static final String LOCAL_APPLICATION_PROPERTIES_RESOURCE = "flink-application-properties-dev.json";
+
+    private static boolean isLocal(StreamExecutionEnvironment env) {
+        return env instanceof LocalStreamEnvironment;
+    }
 
     /**
      * Get configuration properties from Amazon Managed Service for Apache Flink runtime properties
-     * GroupID "FlinkApplicationProperties", or from command line parameters when running locally
+     * or from a local resource when running locally
      */
-    private static ParameterTool loadApplicationParameters(String[] args, StreamExecutionEnvironment env) throws IOException {
-        if (env instanceof LocalStreamEnvironment) {
-            return ParameterTool.fromArgs(args);
+    private static Map<String, Properties> loadApplicationProperties(StreamExecutionEnvironment env)
+            throws IOException {
+        if (isLocal(env)) {
+            LOG.info("Loading application properties from '{}'", LOCAL_APPLICATION_PROPERTIES_RESOURCE);
+            return KinesisAnalyticsRuntime.getApplicationProperties(
+                    Objects.requireNonNull(StreamingJob.class.getClassLoader()
+                            .getResource(LOCAL_APPLICATION_PROPERTIES_RESOURCE)).getPath());
         } else {
-            Map<String, Properties> applicationProperties = KinesisAnalyticsRuntime.getApplicationProperties();
-            Properties flinkProperties = applicationProperties.get("FlinkApplicationProperties");
-            if (flinkProperties == null) {
-                throw new RuntimeException("Unable to load FlinkApplicationProperties properties from runtime properties");
-            }
-            Map<String, String> map = new HashMap<>(flinkProperties.size());
-            flinkProperties.forEach((k, v) -> map.put((String) k, (String) v));
-            return ParameterTool.fromMap(map);
+            LOG.info("Loading application properties from Amazon Managed Service for Apache Flink");
+            return KinesisAnalyticsRuntime.getApplicationProperties();
         }
     }
 
-    private static KafkaSource<String> createKafkaSource(ParameterTool applicationProperties) {
-        String bootstrapServers = applicationProperties.get(MSKBOOTSTRAP_SERVERS_KEY);
-        Preconditions.checkNotNull(bootstrapServers, MSKBOOTSTRAP_SERVERS_KEY + " configuration missing");
+    private static KafkaSource<String> createKafkaSource(Properties applicationProperties) {
+        String bootstrapServers = applicationProperties.getProperty(MSKBOOTSTRAP_SERVERS_KEY);
+        Preconditions.checkNotNull(bootstrapServers, MSKBOOTSTRAP_SERVERS_KEY+ " configuration missing");
 
         KafkaSourceBuilder<String> builder = KafkaSource.<String>builder()
                 .setBootstrapServers(bootstrapServers)
-                .setTopics(applicationProperties.get(KAFKA_SOURCE_TOPIC_KEY, DEFAULT_SOURCE_TOPIC))
-                .setGroupId(applicationProperties.get(KAFKA_CONSUMER_GROUP_ID_KEY, DEFAULT_CONSUMER_GROUP))
+                .setTopics(applicationProperties.getProperty(KAFKA_SOURCE_TOPIC_KEY, DEFAULT_SOURCE_TOPIC))
+                .setGroupId(applicationProperties.getProperty(KAFKA_CONSUMER_GROUP_ID_KEY, DEFAULT_CONSUMER_GROUP))
                 .setStartingOffsets(OffsetsInitializer.earliest()) // Used when the application starts with no state
                 .setValueOnlyDeserializer(new SimpleStringSchema());
 
@@ -73,7 +77,7 @@ public class StreamingJob {
         return builder.build();
     }
 
-    private static void configureConnectorPropsWithConfigProviders(KafkaSourceBuilder<String> builder, ParameterTool appProperties) {
+    private static void configureConnectorPropsWithConfigProviders(KafkaSourceBuilder<String> builder, Properties appProperties) {
         // see https://github.com/aws-samples/msk-config-providers
 
         // define names of config providers:
@@ -83,13 +87,13 @@ public class StreamingJob {
         builder.setProperty("config.providers.secretsmanager.class", "com.amazonaws.kafka.config.providers.SecretsManagerConfigProvider");
         builder.setProperty("config.providers.s3import.class", "com.amazonaws.kafka.config.providers.S3ImportConfigProvider");
 
-        String region = appProperties.get(S3_BUCKET_REGION_KEY);
-        String keystoreS3Bucket = appProperties.get(KEYSTORE_S3_BUCKET_KEY);
-        String keystoreS3Path = appProperties.get(KEYSTORE_S3_PATH_KEY);
-        String truststoreS3Bucket = appProperties.get(TRUSTSTORE_S3_BUCKET_KEY);
-        String truststoreS3Path = appProperties.get(TRUSTSTORE_S3_PATH_KEY);
-        String keystorePassSecret = appProperties.get(KEYSTORE_PASS_SECRET_KEY);
-        String keystorePassSecretField = appProperties.get(KEYSTORE_PASS_SECRET_FIELD_KEY);
+        String region = appProperties.getProperty(S3_BUCKET_REGION_KEY);
+        String keystoreS3Bucket = appProperties.getProperty(KEYSTORE_S3_BUCKET_KEY);
+        String keystoreS3Path = appProperties.getProperty(KEYSTORE_S3_PATH_KEY);
+        String truststoreS3Bucket = appProperties.getProperty(TRUSTSTORE_S3_BUCKET_KEY);
+        String truststoreS3Path = appProperties.getProperty(TRUSTSTORE_S3_PATH_KEY);
+        String keystorePassSecret = appProperties.getProperty(KEYSTORE_PASS_SECRET_KEY);
+        String keystorePassSecretField = appProperties.getProperty(KEYSTORE_PASS_SECRET_FIELD_KEY);
 
         // region, etc..
         builder.setProperty("config.providers.s3import.param.region", region);
@@ -106,9 +110,13 @@ public class StreamingJob {
     public static void main(String[] args) throws Exception {
         // set up the streaming execution environment
         final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
-        final ParameterTool applicationProperties = loadApplicationParameters(args, env);
+        // Load the application properties
+        final Map<String, Properties> applicationProperties = loadApplicationProperties(env);
+        LOG.info("Application properties: {}", applicationProperties);
 
-        KafkaSource<String> source = createKafkaSource(applicationProperties);
+        Properties inputProperties = applicationProperties.get("Input0");
+
+        KafkaSource<String> source = createKafkaSource(inputProperties);
         DataStream<String> input = env.fromSource(source, WatermarkStrategy.noWatermarks(), "Kafka source");
 
         input.print(); // Never print/log every record in a production application
