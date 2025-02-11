8f5e0f90fcd727539fe0d5c23f581f5c632d1770
jeremyber-aws <74626212+jeremyber-aws@users.noreply.github.com>
2025-02-11 06:40:38 -0600

    Iceberg Source + S3 Table Sink (#86)
    * iceberg with s3 tables
    * moved other Iceberg examples into the same folder

---
+++ java/Iceberg/IcebergDataStreamSource/src/main/java/com/amazonaws/services/msf/iceberg/IcebergSinkBuilder.java
@@ -0,0 +1,154 @@
+package com.amazonaws.services.msf.iceberg;
+
+import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.table.data.RowData;
+import org.apache.flink.table.types.logical.RowType;
+import org.apache.flink.util.Preconditions;
+
+import org.apache.avro.generic.GenericRecord;
+import org.apache.iceberg.BaseTable;
+import org.apache.iceberg.NullOrder;
+import org.apache.iceberg.PartitionSpec;
+import org.apache.iceberg.Table;
+import org.apache.iceberg.TableMetadata;
+import org.apache.iceberg.TableOperations;
+import org.apache.iceberg.avro.AvroSchemaUtil;
+import org.apache.iceberg.catalog.Catalog;
+import org.apache.iceberg.catalog.TableIdentifier;
+import org.apache.iceberg.flink.CatalogLoader;
+import org.apache.iceberg.flink.FlinkSchemaUtil;
+import org.apache.iceberg.flink.TableLoader;
+import org.apache.iceberg.flink.sink.AvroGenericRecordToRowDataMapper;
+import org.apache.iceberg.flink.sink.FlinkSink;
+import org.apache.iceberg.flink.util.FlinkCompatibilityUtil;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+
+/**
+ * Wraps the code to initialize an Iceberg sink that uses Glue Data Catalog as catalog
+ */
+public class IcebergSinkBuilder {
+    private static final String DEFAULT_GLUE_DB = "default";
+    private static final String DEFAULT_ICEBERG_TABLE_NAME = "prices_iceberg";
+    private static final String DEFAULT_ICEBERG_SORT_ORDER_FIELD = "accountNr";
+    private static final String DEFAULT_ICEBERG_PARTITION_FIELDS = "symbol";
+    private static final String DEFAULT_ICEBERG_OPERATION = "upsert";
+    private static final String DEFAULT_ICEBERG_UPSERT_FIELDS = "symbol";
+
+
+    /**
+     * If Iceberg Table has not been previously created, we will create it using the Partition Fields specified in the
+     * Properties, as well as add a Sort Field to improve query performance
+     */
+    private static void createTable(Catalog catalog, TableIdentifier outputTable, org.apache.iceberg.Schema icebergSchema, PartitionSpec partitionSpec, String sortField) {
+        // If table has been previously created, we do not do any operation or modification
+        if (!catalog.tableExists(outputTable)) {
+            Table icebergTable = catalog.createTable(outputTable, icebergSchema, partitionSpec);
+            // Modifying newly created iceberg table to have a sort field
+            icebergTable.replaceSortOrder()
+                    .asc(sortField, NullOrder.NULLS_LAST)
+                    .commit();
+            // The catalog.create table creates an Iceberg V1 table. If we want to perform upserts, we need to upgrade the table version to 2.
+            TableOperations tableOperations = ((BaseTable) icebergTable).operations();
+            TableMetadata appendTableMetadata = tableOperations.current();
+            tableOperations.commit(appendTableMetadata, appendTableMetadata.upgradeToFormatVersion(2));
+        }
+    }
+
+    /**
+     * Generate the PartitionSpec, if when creating table, you want it to be partitioned.
+     * If you are doing Upserts in your Iceberg Table, your Equality Fields must be the same as the fields used for Partitioning.
+     */
+    private static PartitionSpec getPartitionSpec(org.apache.iceberg.Schema icebergSchema, List<String> partitionFieldsList) {
+        PartitionSpec.Builder partitionBuilder = PartitionSpec.builderFor(icebergSchema);
+        for (String s : partitionFieldsList) {
+            partitionBuilder = partitionBuilder.identity(s);
+        }
+        return partitionBuilder.build();
+    }
+
+    // Iceberg Flink Sink Builder
+    public static FlinkSink.Builder createBuilder(Properties icebergProperties, DataStream<GenericRecord> dataStream, org.apache.avro.Schema avroSchema) {
+        // Retrieve configuration from application parameters
+        String s3BucketPrefix = Preconditions.checkNotNull(icebergProperties.getProperty("bucket.prefix"), "Iceberg S3 bucket prefix not defined");
+
+        String glueDatabase = icebergProperties.getProperty("catalog.db", DEFAULT_GLUE_DB);
+        String glueTable = icebergProperties.getProperty("catalog.table", DEFAULT_ICEBERG_TABLE_NAME);
+
+        String partitionFields = icebergProperties.getProperty("partition.fields", DEFAULT_ICEBERG_PARTITION_FIELDS);
+        List<String> partitionFieldList = Arrays.asList(partitionFields.split("\\s*,\\s*"));
+
+        String sortField = icebergProperties.getProperty("sort.field", DEFAULT_ICEBERG_SORT_ORDER_FIELD);
+
+        // Iceberg you can perform Appends, Upserts and Overwrites.
+        String icebergOperation = icebergProperties.getProperty("operation", DEFAULT_ICEBERG_OPERATION);
+        Preconditions.checkArgument(icebergOperation.equals("append") || icebergOperation.equals("upsert") || icebergOperation.equals("overwrite"), "Invalid Iceberg Operation");
+
+        // If operation is upsert, we need to specify the fields that will be used for equality in the upsert operation
+        // If the table is partitioned, we must include the partition fields
+        // This is a comma-separated list of fields
+        String upsertEqualityFields = icebergProperties.getProperty("upsert.equality.fields", DEFAULT_ICEBERG_UPSERT_FIELDS);
+        List<String> equalityFieldsList = Arrays.asList(upsertEqualityFields.split("[, ]+"));
+
+
+        // Convert Avro Schema to Iceberg Schema, this will be used for creating the table
+        org.apache.iceberg.Schema icebergSchema = AvroSchemaUtil.toIceberg(avroSchema);
+        // Avro Generic Record to Row Data Mapper
+        MapFunction<GenericRecord, RowData> avroGenericRecordToRowDataMapper = AvroGenericRecordToRowDataMapper.forAvroSchema(avroSchema);
+
+
+        // Catalog properties for using Glue Data Catalog
+        Map<String, String> catalogProperties = new HashMap<>();
+        catalogProperties.put("type", "iceberg");
+        catalogProperties.put("io-impl", "org.apache.iceberg.aws.s3.S3FileIO");
+        catalogProperties.put("warehouse", s3BucketPrefix);
+
+        // Load Glue Data Catalog
+        CatalogLoader glueCatalogLoader = CatalogLoader.custom(
+                "glue",
+                catalogProperties,
+                new org.apache.hadoop.conf.Configuration(),
+                "org.apache.iceberg.aws.glue.GlueCatalog");
+        // Table Object that represents the table in the Glue Data Catalog
+        TableIdentifier outputTable = TableIdentifier.of(glueDatabase, glueTable);
+        // Load created Iceberg Catalog to perform table operations
+        Catalog catalog = glueCatalogLoader.loadCatalog();
+
+
+        // Based on how many fields we want to partition, we create the Partition Spec
+        PartitionSpec partitionSpec = getPartitionSpec(icebergSchema, partitionFieldList);
+        // We create the Iceberg Table, using the Iceberg Catalog, Table Identifier, Schema parsed in Iceberg Schema Format and the partition spec
+        createTable(catalog, outputTable, icebergSchema, partitionSpec, sortField);
+        // Once the table has been created in the job or before, we load it
+        TableLoader tableLoader = TableLoader.fromCatalog(glueCatalogLoader, outputTable);
+        // Get RowType Schema from Iceberg Schema
+        RowType rowType = FlinkSchemaUtil.convert(icebergSchema);
+
+        // Iceberg DataStream sink builder
+        FlinkSink.Builder flinkSinkBuilder = FlinkSink.<org.apache.avro.generic.GenericRecord>builderFor(
+                        dataStream,
+                        avroGenericRecordToRowDataMapper,
+                        FlinkCompatibilityUtil.toTypeInfo(rowType))
+                .tableLoader(tableLoader);
+
+        // Returns the builder for the selected operation
+        switch (icebergOperation) {
+            case "upsert":
+                // If operation is "upsert" we need to set up the equality fields
+                return flinkSinkBuilder
+                        .equalityFieldColumns(equalityFieldsList)
+                        .upsert(true);
+            case "overwrite":
+                return flinkSinkBuilder
+                        .overwrite(true);
+            default:
+                return flinkSinkBuilder;
+        }
+    }
+
+}
---
+++ java/Iceberg/S3TableSink/README.md
@@ -0,0 +1,98 @@
+# Flink Iceberg Sink using DataStream API
+
+* Flink version: 1.19.0
+* Flink API: DataStream API
+* Iceberg 1.6.1
+* Language: Java (11)
+* Flink connectors: [DataGen](https://nightlies.apache.org/flink/flink-docs-release-1.20/docs/connectors/datastream/datagen/) 
+   and [Iceberg](https://iceberg.apache.org/docs/latest/flink/)
+
+This example demonstrate how to use
+[Flink Iceberg Sink Connector](https://iceberg.apache.org/docs/latest/flink-writes/) with S3 Tables.
+
+For simplicity, the application generates synthetic data, random stock prices, internally. 
+Data is generated as AVRO Generic Record, simulating a real source, for example a Kafka Source, that receives records 
+serialized with AVRO.
+
+### Prerequisites
+
+### Create a Table Bucket
+The sample application expects the S3 Table Bucket to exist and to have the ARN in the local environment:
+```bash
+aws s3tables create-table-bucket --name flink-example
+{
+      "arn": "arn:aws:s3tables:us-east-1:111122223333:bucket/flink-example"
+
+}
+```
+
+If you already did this, you can query to get the ARN like this:
+
+```bash
+aws s3tables list-table-buckets
+```
+
+This will show you the list of table buckets. Select the one you wish to write to and paste it into the config file in this project.
+
+
+#### IAM Permissions
+
+The application must have IAM permissions to:
+* Write and Read from the S3 Table
+
+### Runtime configuration
+
+When running on Amazon Managed Service for Apache Flink the runtime configuration is read from Runtime Properties.
+
+When running locally, the configuration is read from the
+[resources/flink-application-properties-dev.json](./src/main/resources/flink-application-properties-dev.json) file.
+
+Runtime parameters:
+
+| Group ID  | Key                      | Default           | Description                                                                                                         |
+|-----------|--------------------------|-------------------|---------------------------------------------------------------------------------------------------------------------|
+| `DataGen` | `records.per.sec`        | `100.0`           | Records per second generated.                                                                                       |
+| `Iceberg` | `table.bucket.arn`       | (mandatory)       | ARN of the S3 bucket, e.g., `arn:aws:s3tables:region:account-id:bucket/bucket-name`                                 |
+| `Iceberg` | `catalog.db`             | `test_from_flink` | Name of the S3 table database.                                                                                      |
+| `Iceberg` | `catalog.table`          | `test_table`      | Name of the S3 table.                                                                                               |
+| `Iceberg` | `partition.fields`       | `symbol`          | Comma separated list of partition fields.                                                                           |
+| `Iceberg` | `sort.field`             | `timestamp`       | Sort field.                                                                                                         |
+| `Iceberg` | `operation`              | `upsert`          | Iceberg operation. One of `upsert`, `append` or `overwrite`.                                                        |
+| `Iceberg` | `upsert.equality.fields` | `symbol`          | Comma separated list of fields used for upsert. It must match partition fields. Required if `operation` = `upsert`. |
+
+### Checkpoints
+
+Checkpointing must be enabled. Iceberg commits writes on checkpoint.
+
+When running locally, the application enables checkpoints programmatically, every 10 seconds.
+When deployed to Managed Service for Apache Flink, checkpointing is controlled by the application configuration.
+
+
+### Known limitations
+
+At the moment there are current limitations concerning Flink Iceberg integration with S3 Tables:
+* * Currently, this example needs to be in Flink v1.19, v1.20 isn't supported with the S3 Table Sink yet.
+* Doesn't support Iceberg Table with hidden partitioning
+* Doesn't support adding columns, removing columns, renaming columns or changing columns.
+
+### Schema and schema evolution
+
+The application must "know" the AVRO schema on start. 
+The schema cannot be dynamically inferred based on the incoming records, for example using a schema registry. 
+This is due to a limitation of the Flink Iceberg integration, that requires knowing the table schema upfront.
+
+This implementation does support schema evolution in the incoming data, as long as new schema versions are FORWARD compatible.
+Schema changes are not propagated to Iceberg. 
+As long as the schema of incoming records is FORWARD compatible, the application deserializes incoming records using
+the schema it knows. Any new field in the incoming record is discarded.
+
+In this example, the schema is loaded from a schema definition file, [price.avsc](./src/main/resources/price.avsc) embedded 
+with the application. 
+It is technically possible to fetch the schema on application start from an external source, like a schema registry or a
+schema definition file in an S3 bucket. This is beyond the scope of this example.
+
+### Running locally, in IntelliJ
+
+You can run this example directly in IntelliJ, without any local Flink cluster or local Flink installation.
+
+See [Running examples locally](https://github.com/nicusX/amazon-managed-service-for-apache-flink-examples/blob/main/java/running-examples-locally.md) for details.
